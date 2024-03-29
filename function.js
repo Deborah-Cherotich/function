1541f1f63863d848665fe7640e67c84b36598f01
Deborah Chrotich <cherotichdeborahb@gmail.com>
2024-03-29 19:44:58 +0300

    add files

---
+++ function.js
@@ -0,0 +1,43 @@
+//reversing function of a string
+function reverseStrings(){
+    const place = ["School","Home","Church","Town"]; 
+    const place2 = place.reverse();  
+    console.log({plce2});
+}
+
+//Checking the 
+function checkNumbers(){
+    let nums= [2,3,4,5]
+    if(nums === positive){
+        console.log("positve")}
+  
+    else{
+        console.log("Zero");
+    }
+}
+
+// Using js fuctions and an array of Number,return positive if an element within an array is positiveve,negative if an element is negative ,else zero
+
+//3
+
+
+    
+
+multiply=[2,3,4,5]
+function multiply (nums){
+    nums.forEach((nums)=>
+    {
+        console.log(num *2);
+    });
+}
+// array of numbers
+arrayNumbers([30,40,20,50,]);
+
+function arrayNumbers(arr) {
+const firstFour = arr.slice(0, 4).map(item => item * 8);
+const lastTwo = arr.slice(-2).map(item => item + 5);
+const newArr = firstFour.concat(lastTwo);
+console.log(newArr);
+}
+
+ 
